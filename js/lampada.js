41d4ad879121278a54583aa0978064bd6195fbed
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-09-22 21:12:15 -0300

    Update lampada.js

--- js/lampada.js
+++ js/lampada.js
@@ -10,33 +10,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function quarto() {
         if (backgroundColor == "yellow" && contador < 5) {
+            backgroundColor = "black";
             luz.style.backgroundColor = "black";
             luz.style.boxShadow = "none";
             lampada.style.backgroundColor = "rgb(102, 3, 3)";
             lampada.style.filter = "brightness(30%)";
-            backgroundColor = "black";
             quebrado.style.display = "none";
             chave.style.display = "none";
             music.play();
             contador++;
         } else if (backgroundColor == "black" && contador < 5) {
+            backgroundColor = "yellow";
             luz.style.backgroundColor = "yellow";
             luz.style.boxShadow = "0px 0px 90px 30px rgb(253, 250, 162)";
             lampada.style.backgroundColor = "brown";
             lampada.style.filter = "brightness(100%)";
-            backgroundColor = "yellow";
             quebrado.style.display = "none";
             chave.style.display = "none";
             music.play();
             contador++;
         } else if (backgroundColor == "yellow" && contador == 5) {
+            backgroundColor = "black";
             luz.style.backgroundColor = "black";
             luz.style.boxShadow = "none";
             lampada.style.backgroundColor = "rgb(102, 3, 3)";
             lampada.style.filter = "brightness(30%)";
             quebrado.style.display = "block";
             chave.style.display = "block";
-            backgroundColor = "black";
             music.play();
             contador = 0;
         }
